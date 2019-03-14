a240abd7008edd1e0752f015cc49babfef925fe0
Karolis Samuolis <Tinshi@users.noreply.github.com>
2019-03-14 22:06:11 +0200

    Changed some ui

--- SensorManagementWeb/ClientApp/src/app/models/Item.ts
+++ SensorManagementWeb/ClientApp/src/app/models/Item.ts
@@ -1,7 +1,14 @@
 export interface Item {
   id?: string;
+  name?: string;
   type?: string;
   status?: string;
   power?: string;
+  power_u?: string;
   temperature?: string;
+  temperature_u?: string;
+  amperage?: string;
+  amperage_u?: string;
+  voltage?: string;
+  voltage_u?: string;
 }

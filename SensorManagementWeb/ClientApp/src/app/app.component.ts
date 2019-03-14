42aaa12b9961016f5842e6886fce4ab53e6a4892
Karolis Samuolis <Tinshi@users.noreply.github.com>
2019-03-14 19:33:17 +0200

    Fixing merge conflicts

--- SensorManagementWeb/ClientApp/src/app/app.component.ts
+++ SensorManagementWeb/ClientApp/src/app/app.component.ts
@@ -15,39 +15,5 @@ import { map } from 'rxjs/operators';
 })
 
 export class AppComponent implements OnInit {
-  user: firebase.User;
-  panelOpenState = false;
-  items: Item[];
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-  constructor(private breakpointObserver: BreakpointObserver, private service: LoginService,
-              private afAuth: AngularFireAuth, private router: Router, private itemService: ItemService) {
 
-  }
-
-  ngOnInit() {
-    this.itemService.getItems().subscribe(items => {
-      this.items = items;
-    });
-    this.service.getLoggedInUser()
-      .subscribe( user => {
-        console.log( user );
-        this.user = user;
-        if ( user != null) {
-          this.router.navigate(['main']);
-        }
-      });
-    }
-
-    loginGoogle() {
-      this.service.login();
-      this.router.navigate(['main']);
-    }
-
-    logout() {
-      this.service.logout();
-      this.router.navigate(['/']);
-    }
 }
